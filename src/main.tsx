f388cc9fbad13e2a024ea7a2516703304d9a5926
Daniel <df1.serrano@gmail.com>
2025-02-22 21:51:36 -0500

    styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+// Import base styles first
 import './styles/globals.css'
+// Then component styles
 import './index.css'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
